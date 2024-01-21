dc1418c672f620f19c4e33dd8a739a7a397a6ba9
Filip-Povidernyi <p.poviderniy@gmail.com>
2024-01-21 19:18:27 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const elems = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name = delay]'),
+  step: document.querySelector('[name = step]'),
+  amount: document.querySelector('[name = amount]'),
+  submitBtn: document.querySelector
+};
+
+elems.form.addEventListener('click', onPromiseCreate);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onPromiseCreate(e) {
+  e.preventDefault();
+
+  let valueDelay = Number(elems.delay.value);
+  let step = Number(elems.step.value);
+  let amount = Number(elems.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    let promiseDelay
+    if (i === 1) {
+      promiseDelay = valueDelay;
+    } else {
+      promiseDelay = valueDelay + step * (i - 1);
+    }
+
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        iziToast.info({
+          title: '✅',
+          message: `Fulfilled promise ${position} in ${delay}ms`,
+          position: 'topRight',
+        });
+      })
+      .catch(({ position, delay }) => {
+        iziToast.error({
+          title: '❌',
+          message: `Rejected promise ${position} in ${delay}ms`,
+          position: 'topRight',
+        });
+      });
   }
 }
